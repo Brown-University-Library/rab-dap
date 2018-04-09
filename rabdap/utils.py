34e07192c90349b3454515afea5a6eee209481f0
Steven McCauley <steven_mccauley@brown.edu>
2018-04-09 12:52:15 -0400

    Refactor LdapClient for better connections, searching

--- rabdap/utils.py
+++ rabdap/utils.py
@@ -5,42 +5,51 @@ import ldap3
 class LdapClient:
     def __init__(self, cfg):
         self.throttle = cfg['LDAP_THROTTLE']
-        self.server_addr = cfg['LDAP_SERVER']
-        self.user = cfg['LDAP_USER']
-        self.passw = cfg['LDAP_PASSWORD']
-        self.user_grp = cfg['LDAP_USERGROUP']
         self.ldap_attrs = [ 'brownBruID','brownShortID',
             'brownUUID','displayName','mail']
-        self.server = ldap3.Server(self.server_addr)
-        self.conn = None
-        self.is_open = False
-        self.is_closed = True
-
-    def open(self):
+        self.server = ldap3.Server(cfg['LDAP_SERVER'])
         self.conn = ldap3.Connection(
             self.server,
             'cn={0},ou={1},dc=brown,dc=edu'.format(
-                self.user, self.user_grp),
-            self.passw, auto_bind=True)
-        self.is_open = True
-        self.is_closed = False
+                cfg['LDAP_USER'], cfg['LDAP_USERGROUP']),
+            cfg['LDAP_PASSWORD'])
+        self.opened = False
+        self.closed = True
+
+    def open(self):
+        self.conn.bind() 
+        self.opened = self.conn.bound
+        self.closed = self.conn.closed
 
     def close(self):
         self.conn.unbind()
-        self.is_open = False
-        self.is_closed = True
+        self.opened = self.conn.bound
+        self.closed = self.conn.closed
 
-    def search_bruids(self, bruids):
-        formatted = [ '(brownbruid={0})'.format(b) for b in bruids ]
-        chunked = chunk_list(formatted, 100)
+    def search(self, searchTerms, field='bruid'):
+        field_map = {
+            'bruid' : 'brownbruid',
+            'shortid' : 'brownshortid',
+            'uuid' : 'brwonuuid',
+            'name' : 'displayname',
+            'email' : 'mail'
+        }
+        try:
+            search_field = field_map[field]
+        except:
+            raise("Unrecognized LDAP field")
+        if isinstance(searchTerms, list):
+            formatted = [ '({0}={1})'.format(search_field, s)
+                for s in searchTerms ]
+            chunked = chunk_list(formatted, 100)
+            query = [ '(|{0})'.format(''.join(c)) for c in chunked ]
+        else:
+            query = [ '({0}={1})'.format(search_field, searchTerms) ]
         ldap_data = []
-        for chunk in chunked:
+        for q in query:
             time.sleep(self.throttle)
-            print('Querying')
-            or_str = '(|{0})'.format(''.join(chunk))
             resp = self.conn.search('ou=people,dc=brown,dc=edu',
-                        or_str,
-                        attributes=self.ldap_attrs)
+                        q, attributes=self.ldap_attrs)
             if resp == True:
                 entries = [ json.loads( e.entry_to_json() )
                             for e in self.conn.entries ]
@@ -49,7 +58,6 @@ class LdapClient:
                 continue
         return ldap_data
 
-# begin Data transformations
 def chunk_list(lst, size):
     chunked = []
     for i in range(0, len(lst), size):
